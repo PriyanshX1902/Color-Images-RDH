646bf7688ca1ca6a273906fe5c7033bd5e1ce838
PriyanshX1902 <priyansh.bhatnagar@rediff.com>
2023-04-01 12:10:44 +0530

    Implemented Edge detection

--- decode_skewed_histogram_shifting.m
+++ decode_skewed_histogram_shifting.m
@@ -1,10 +1,17 @@
-function [mod_P, re_message]=decode_skewed_histogram_shifting(P,half)
+function [mod_P, re_message]=decode_skewed_histogram_shifting(P,half, ref_P, channel)
 [n m]=size(P);
 %border side information extraction
 message_length_max=floor(log2(n*m));
-b_side_info = P(1:message_length_max+2);
-bi_message_length=bi2de(mod(b_side_info(1:end-2),2));
-pred_method=bi2de(mod(b_side_info(end-1:end),2));
+b_side_info = P(1:message_length_max+6);
+bi_message_length=bi2de(mod(b_side_info(1:end-6),2));
+pred_method=bi2de(mod(b_side_info(end-5:end-4),2));
+threshold = bi2de(mod(b_side_info(end-3:end),2));
+DM = zeros(n, m);
+DH = zeros(n, m);
+DV = zeros(n, m);
+DD = zeros(n, m);
+DAD = zeros(n, m);
+DE = zeros(n, m);
 
 
 LC=zeros(n,m);
@@ -14,11 +21,26 @@ for i1=2:n-1
         if rem(i1+j1,2)==half
             counter=counter+1;
             W=P(i1,j1-1);N=P(i1-1,j1);E=P(i1,j1+1);S=P(i1+1,j1);
-            LC(i1,j1)=abs(N-E)+abs(E-S)+abs(S-W)+abs(W-N)+abs(N-S)+abs(E-W);
+            LC(i1,j1)=(abs(N-E)+abs(E-S)+abs(S-W)+abs(W-N)+abs(N-S)+abs(E-W))/6;
+            neighbors = zeros(1, 8);
+            neighbors(1, 1) = ref_P(i1-1, j1); %N
+            neighbors(1, 2) = ref_P(i1, j1-1); %W
+            neighbors(1, 3) = ref_P(i1-1, j1-1); %NW
+            neighbors(1, 4) = ref_P(i1+1, j1-1); %SW
+            neighbors(1, 5) = ref_P(i1-1, j1+1); %NE
+            neighbors(1, 6) = ref_P(i1+1, j1); %S
+            neighbors(1, 7) = ref_P(i1, j1+1); %E
+            neighbors(1, 8) = ref_P(i1+1, j1+1); %SE
+            DM(i1, j1) = abs((sum(neighbors)*(1/8))-ref_P(i1, j1));
+            DH(i1, j1) = abs(((neighbors(1, 2)+neighbors(1, 7))/2)-ref_P(i1, j1));
+            DV(i1, j1) = abs(((neighbors(1, 1)+neighbors(1, 6))/2)-ref_P(i1, j1));
+            DD(i1, j1) = intmax;%abs(((neighbors(1, 3)+neighbors(1, 8))/2)-ref_P(i1, j1));
+            DAD(i1, j1) = intmax;%abs(((neighbors(1, 4)+neighbors(1, 5))/2)-ref_P(i1, j1));
+            DE(i1, j1) = min([DH(i1, j1) DV(i1, j1) DD(i1, j1) DAD(i1, j1)]);
         end
     end
 end
-pixel_profile=zeros(counter,7);
+pixel_profile=zeros(counter,18);
 counter=0;
 
 for i1=2:n-1
@@ -26,12 +48,23 @@ for i1=2:n-1
         if rem(i1+j1,2)==half
             counter=counter+1;
             W=P(i1,j1-1);N=P(i1-1,j1);E=P(i1,j1+1);S=P(i1+1,j1);
-            W_LC=LC(i1,j1)+LC(i1-1,j1-1)+LC(i1-1,j1+1)+LC(i1+1,j1-1)+LC(i1+1,j1+1);
-            pixel_profile(counter,:)=[W_LC P(i1,j1) N W S E (j1-1)*n+i1];
+            NW=P(i1-1,j1-1);NE=P(i1-1,j1+1);SE=P(i1+1,j1+1);SW=P(i1+1,j1-1);
+            w1 = power(abs(N-E)-LC(i1, j1), 2);
+            w2 = power(abs(W-S)-LC(i1, j1), 2);
+            w3 = power(abs(N-W)-LC(i1, j1), 2);
+            w4 = power(abs(S-E)-LC(i1, j1), 2);
+            w5 = power(abs(N-S)-LC(i1, j1), 2);
+            w6 = power(abs(W-E)-LC(i1, j1), 2);
+            W_LC=power(sum([w1 w2 w3 w4 w5 w6])/6, 1/2);%+LC(i1-1,j1-1)+LC(i1-1,j1+1)+LC(i1+1,j1-1)+LC(i1+1,j1+1);
+            if DM(i1, j1)-DE(i1, j1)>threshold
+                pixel_profile(counter, :) = [W_LC P(i1, j1) ref_P(i1, j1) DH(i1, j1) DV(i1, j1) DD(i1, j1) DAD(i1, j1) N W S E NW NE i1 j1 DM(i1, j1) DE(i1, j1) (j1-1)*n+i1];
+            else
+                pixel_profile(counter, :)=[W_LC P(i1,j1) N W S E -1 -1 -1 -1 -1 -1 -1 i1 j1 DM(i1, j1) DE(i1, j1) (j1-1)*n+i1];
+            end
         end
     end
 end
-sorted_pixel_profile=sortrows(pixel_profile,1);
+sorted_pixel_profile=sortrows(pixel_profile, 1);
 
 if half == 1
     message_length=bi_message_length-round(bi_message_length/2);
@@ -40,33 +73,52 @@ else
 end
 
 counter=length(sorted_pixel_profile);
+
 location=zeros(1,counter);
 mod_P=P;
 ec=0;
 final_set=0;
 temp=zeros(1,counter);
+
 for i1=1:counter
     current_set=sorted_pixel_profile(i1,:);
-    loc_y=current_set(7);
-    if loc_y > message_length_max+2
+    loc_y=current_set(18);
+    isEdge = current_set(16)-current_set(17);
+    if loc_y > message_length_max+6
         %Prediction
-        if pred_method==1
-            sorted_pixels=sort(current_set(3:6),'descend');
-            Pred_h=round(sum(sorted_pixels(1)));
-            Pred_l=round(sum(sorted_pixels(4)));
-        elseif pred_method==2
-            sorted_pixels=sort(current_set(3:6),'descend');
-            Pred_h=round(sum(sorted_pixels(1:2))/2);
-            Pred_l=round(sum(sorted_pixels(3:4))/2);
-        elseif pred_method==3
-            sorted_pixels=sort(current_set(3:6),'descend');
-            Pred_h=round(sum(sorted_pixels(1:3))/3);
-            Pred_l=round(sum(sorted_pixels(2:4))/3);
-        elseif pred_method==4
-            Pred_h=round(sum(current_set(3:6))/4);
-            Pred_l=Pred_h-1;
-        else 
-            pause
+        if isEdge>threshold
+            if current_set(17)==current_set(4)
+                Pred_h = max([current_set(9) current_set(11)]);
+                Pred_l = min([current_set(9) current_set(11)]);
+            elseif current_set(17)==current_set(5)
+                Pred_h = max([current_set(8) current_set(10)]);
+                Pred_l = min([current_set(8) current_set(10)]);
+            elseif current_set(17)==current_set(6)
+                Pred_h = max([current_set(12) current_set(15)]);
+                Pred_l = min([current_set(12) current_set(15)]);
+            elseif current_set(17)==current_set(7)
+                Pred_h = max([current_set(13) current_set(14)]);
+                Pred_l = min([current_set(13) current_set(14)]);
+            end
+        else
+            if pred_method==1
+                sorted_pixels=sort(current_set(3:6),'descend');
+                Pred_h=round(sum(sorted_pixels(1)));
+                Pred_l=round(sum(sorted_pixels(4)));
+            elseif pred_method==2
+                sorted_pixels=sort(current_set(3:6),'descend');
+                Pred_h=round(sum(sorted_pixels(1:2))/2);
+                Pred_l=round(sum(sorted_pixels(3:4))/2);
+            elseif pred_method==3
+                sorted_pixels=sort(current_set(3:6),'descend');
+                Pred_h=round(sum(sorted_pixels(1:3))/3);
+                Pred_l=round(sum(sorted_pixels(2:4))/3);
+            elseif pred_method==4
+                Pred_h=round(sum(current_set(3:6))/4);
+                Pred_l=Pred_h-1;
+            else
+                pause;
+            end
         end
         if Pred_h==Pred_l
             Pred_l=Pred_l-1;
@@ -76,10 +128,23 @@ for i1=1:counter
         pixel=mod_P(loc_y);
         if message_length >= ec+1
             [ec, pixel, re_m]=prediction(ec,pixel,pe,-1);
-            pe=pixel-Pred_h;
-            if isempty(re_m)==0
-                re_message(ec)=re_m;
+            if flag==1
+                if i1==counter
+                    if isempty(re_m)==0
+                        ec = ec-1;
+                    end
+                else
+                    if isempty(re_m)==0
+                        re_message(ec)=re_m;
+                    end
+                end
+            else
+                if isempty(re_m)==0
+                    re_message(ec)=re_m;
+                end
             end
+            pe=pixel-Pred_h;
+            
             if message_length ~= ec
                 [ec, pixel, re_m]=prediction(ec,pixel,pe,1);
                 if isempty(re_m)==0
--- main_decode.m
+++ main_decode.m
@@ -1,27 +1,61 @@
-function [re_original, re_message]=main_decode(watermarked_image)
+function [re_original, re_message]=main_decode(watermarked_image, em_message)
 
-[re_mod_P2, re_message2]=decode_skewed_histogram_shifting(watermarked_image,1);
-[re_mod_P1, re_message1]=decode_skewed_histogram_shifting(re_mod_P2,0);
-re_message=[re_message1 re_message2];
+[re_mod_P2_green, re_message2]=decode_skewed_histogram_shifting(watermarked_image(:, :, 2),1, watermarked_image(:, :, 1), 3);
+[re_mod_P1_green, re_message1]=decode_skewed_histogram_shifting(re_mod_P2_green,0,watermarked_image(:, :, 1) , 3);
+[re_mod_P2_blue, re_message4]=decode_skewed_histogram_shifting(watermarked_image(:, :, 3),1, re_mod_P1_green, 2);
+[re_mod_P1_blue, re_message3]=decode_skewed_histogram_shifting(re_mod_P2_blue,0, re_mod_P1_green, 2);
+[re_mod_P2_red, re_message6]=decode_skewed_histogram_shifting(watermarked_image(:, :, 1),1, re_mod_P1_green, 1);
+[re_mod_P1_red, re_message5]=decode_skewed_histogram_shifting(re_mod_P2_red,0, re_mod_P1_green, 1);
+re_message=[re_message5 re_message6 re_message3 re_message4 re_message1 re_message2];
+re_message_green = [re_message1 re_message2];
+re_message_blue = [re_message3 re_message4];
+re_message_red = [re_message5 re_message6];
+len = floor(length(em_message)/3);
+
 
 %Undo preprocessing
-[re_n re_m]=size(watermarked_image);
+[re_n re_m]=size(watermarked_image(:,:,1));
 re_message_length_max=floor(log2(re_n*re_m));
 re_length_loc_map_max=ceil(log2((re_n-2)*(re_m-2)));
 
 %side information
-re_border_pixel=re_message(1:re_message_length_max+2);
-
-start_pos=re_message_length_max+2+1;
-re_location_map_length=bi2de(re_message(start_pos:start_pos+re_length_loc_map_max-1));
-
+re_border_pixel_red=re_message(1:re_message_length_max+6);
+start_pos=re_message_length_max+6+1;
+re_location_map_length_red=bi2de(re_message(start_pos:start_pos+re_length_loc_map_max-1));
 start_pos=start_pos+re_length_loc_map_max;
-re_location_map=re_message(start_pos:start_pos+re_location_map_length-1);
-re_message(1:start_pos+re_location_map_length-1)=[];
+re_location_map_red=re_message(start_pos:start_pos+re_location_map_length_red-1);
+re_message(1:start_pos+re_location_map_length_red-1)=[];
+
+re_border_pixel_green=re_message(1:re_message_length_max+6);
+start_pos=re_message_length_max+6+1;
+re_location_map_length_green=bi2de(re_message(start_pos:start_pos+re_length_loc_map_max-1));
+start_pos=start_pos+re_length_loc_map_max;
+re_location_map_green=re_message(start_pos:start_pos+re_location_map_length_green-1);
+re_message(1:start_pos+re_location_map_length_green-1)=[];
+
+re_border_pixel_blue=re_message(1:re_message_length_max+6);
+start_pos=re_message_length_max+6+1;
+re_location_map_length_blue=bi2de(re_message(start_pos:start_pos+re_length_loc_map_max-1));
+start_pos=start_pos+re_length_loc_map_max;
+re_location_map_blue=re_message(start_pos:start_pos+re_location_map_length_blue-1);
+re_message(1:start_pos+re_location_map_length_blue-1)=[];
 %Recover border pixel
-re_mod_P1(1:re_message_length_max+2)=bitxor(bitxor(re_mod_P1(1:re_message_length_max+2),mod(re_mod_P1(1:re_message_length_max+2),2)),re_border_pixel);
-if isempty(re_location_map)
-    re_original=re_mod_P1;
+re_mod_P1_red(1:re_message_length_max+6)=bitxor(bitxor(re_mod_P1_red(1:re_message_length_max+6),mod(re_mod_P1_red(1:re_message_length_max+6),2)),re_border_pixel_red);
+re_mod_P1_green(1:re_message_length_max+6)=bitxor(bitxor(re_mod_P1_green(1:re_message_length_max+6),mod(re_mod_P1_green(1:re_message_length_max+6),2)),re_border_pixel_green);
+re_mod_P1_blue(1:re_message_length_max+6)=bitxor(bitxor(re_mod_P1_blue(1:re_message_length_max+6),mod(re_mod_P1_blue(1:re_message_length_max+6),2)),re_border_pixel_blue);
+if 1
+    re_original_red=re_mod_P1_red;
 else
-    [re_original]=re_preproces_image(re_mod_P1,re_location_map);
+    [re_original_red]=re_preproces_image(re_mod_P1_red,re_location_map_red);
 end
+if 1
+    re_original_green=re_mod_P1_green;
+else
+    [re_original_green]=re_preproces_image(re_mod_P1_green,re_location_map_green);
+end
+if 1
+    re_original_blue=re_mod_P1_blue;
+else
+    [re_original_blue]=re_preproces_image(re_mod_P1_blue,re_location_map_blue);
+end
+re_original = cat(3, re_original_red, re_original_green, re_original_blue);
